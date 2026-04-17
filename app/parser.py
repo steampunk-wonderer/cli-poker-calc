a415c1e20fe63e3344a174657ac3f1aa441d9a57
Alex Papadopoulos <alexandrospap8@gmail.com>
2026-04-17 19:17:09 +0300

    full_string_to_card_strings and some incomplete parser stuff

--- app/parser.py
+++ app/parser.py
@@ -1,4 +1,83 @@
 import argparse
+from app.models import Suit
+import re
 
+values_dict = { 
+    '2':2,
+    '3':3,
+    '4':4,
+    '5':5,
+    '6':6,
+    '7':7,
+    '8':8,
+    '9':9,
+    '10':10,
+    'J':11,
+    'Q':12,
+    'K':13,
+    'A':14,
+}
+
+suits_dict = { 
+    'h':Suit.HEARTS,
+    'c':Suit.CLUBS,
+    's':Suit.SPADES,
+    'd':Suit.DIAMONDS
+}
+
+def full_string_to_card_strings(full_str:str)->list[str]:
+    """
+    10s7cQh2cAh -> [10s,7c,Qh,2c,Ah]
+    """
+    print("full_string_to_card_strings function running")
+    if not full_str: 
+        raise TypeError(f"Invalid full_str:{full_str}")
+    if not isinstance(full_str,str): 
+        raise TypeError(f"full_str:{full_str} must be a string")
+    print("full_str",full_str)
+    suits = suits_dict.keys()
+    print('suits',suits)
+    pattern = rf"[hcsd]"
+    matches = re.finditer(pattern,full_str)
+
+    indexes_suits = [m.start() for m in matches]
+    indexes_suits = [-1] + indexes_suits
+    print('indexes_suits: ',indexes_suits)
+    card_strings = []
+    for i in range(0,len(indexes_suits)-1): 
+        card_str = full_str[indexes_suits[i]+1:indexes_suits[i+1]+1]
+        card_strings.append(card_str)
+    print(card_strings)
+    return card_strings
+
+
+    
+    
+
+def card_string_to_single_Card(card_string:str):
+    print("card_string_to_Card function running")
+    if not card_string:
+        raise ValueError(f"Invalid card_string:{card_string}")
+    if not isinstance(card_string,str):
+        raise TypeError(f"card_string:{card_string} must be a string")
+    card_string = card_string.strip()
+    if len(card_string) != 2 or len(card_string) !=3 : 
+        raise ValueError(f"card_string:{card_string} must have len 2 or 3")
+    if card_string[-1] not in suits_dict or card_string[:-1] not in values_dict: 
+        raise ValueError(f"card_string:{card_string} must be valuesuit like 2h")
+    
+    suit_str = card_string[-1]
+    value_str = card_string[:-1]
+    print('suit_str: ',suit_str)
+    print('value_str: ',value_str)
+     
 def parse_game_input(args:argparse.Namespace): 
-    pass
+    print("inside the parse_game_input function")
+    print("args: ",args)
+    players = args.players
+    player_cards = args.player_cards
+    other_players_cards = args.other_players_cards
+    community_cards = args.community_cards
+    #PLAYER CARDS
+    # card_string_to_Card(player_cards)
+
--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 from app.models import Suit,Card
 import sys
 import argparse
+from app.parser import parse_game_input,full_string_to_card_strings
 
 def main(): 
+    #-----------------------------------------------#
+    #-----------------------------------------------#
+    #PARSING
     parser = argparse.ArgumentParser() #create parser
     parser.add_argument("--players", type=int,default=1)
     parser.add_argument("--player_cards",default=None)
-    parser.add_argument("--other_player_cards",default=None)
+    parser.add_argument("--other_players_cards",nargs="*",default=None)
     parser.add_argument("--community_cards",default=None)
-
-
-
-
     args = parser.parse_args() #parse
-    print('args: ',args)
-    print("type: ",type(args))
 
+    parse_game_input(args)
 
-
-    card = Card(Suit.HEARTS,4)
-    print(card)
-    # print("sys.argv: ",sys.argv)
-    
+    test_str = "Ac"
+    full_string_to_card_strings("10s7cQh2cAhKh")
+    #-----------------------------------------------#
+    #-----------------------------------------------#)
 
 if __name__ == "__main__": 
     main()
