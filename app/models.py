5737cf10dcaf74d6600c595c7bf9b6ec595d9583
Alex Papadopoulos <alexandrospap8@gmail.com>
2026-04-17 19:36:04 +0300

    mini bug fix

--- app/models.py
+++ app/models.py
@@ -11,8 +11,8 @@ class Card:
     def __init__(self,suit:Suit,value:int)->None:
         if not isinstance(suit,Suit):
             raise TypeError("suit must be Suit")
-        if value > 13 or value < 1:
-            raise ValueError("value must be from 1-13")
+        if value > 14 or value < 2:
+            raise ValueError("value must be from 2-14")
         self.suit = suit
         self.value = value
 
